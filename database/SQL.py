bbc0d55f231187cc24b5780b9d705731299ee8e4
DivMoyal <divmoyal@gmail.com>
2024-11-21 15:15:28 +0200

    Delete database directory

--- database/SQL.py
+++
@@ -1,7 +0,0 @@
-import  pyodbc
-connection_string = ("DRIVER={SQL Server);"
-                     'PORT=1433;'
-                     'SERVER = LAPTOP-N997RI3E\SQLEXPRESS;'
-                     'DATABASE = NORTHWIND;'
-                     'Trusted_connection=yes;'
-
